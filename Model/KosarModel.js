0d4471a082155aa3baafbd4d38d6674814595205
Blaj3n <benceulrich@gmail.com>
2022-10-25 14:08:32 +0200

    class KosarModel + getKosarTomb

--- Model/KosarModel.js
+++ Model/KosarModel.js
@@ -1,4 +1,16 @@
-class KosarModel{
-    #kosarTomb=[];
-    
+class KosarModel {
+  #kosarTomb = [];
+
+  constructor() {
+    console.log("KosarModel");
+  }
+    adatKosarbarak(adat) {
+    console.log("Kosárba Rak", adat);
+  }
+
+  getKosarTomb(){
+    return this.#kosarTomb;
+  }
 }
+
+export default KosarModel;
